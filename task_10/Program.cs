5dcabef79810a6d32efef019a9624c639ee4d30c
Дмитрий <107067123+Dmitriy-Dmiriy@users.noreply.github.com>
2022-06-29 18:30:30 +0300

    task2

---
+++ task_10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int FindSecondDigit(int num)
+
+{
+      int d = num / 10;
+      while( d > 10)
+      {
+            d = d -10;
+      }
+
+        return d;
+}
+
+Console.Write("Введите трёхзначное число ->  ");
+int n = Convert.ToInt32(Console.ReadLine());
+int second = FindSecondDigit(n);
+Console.WriteLine("Вторая цифра -> " + second); 
+
+/*Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+*/
---
+++ task_13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+// заданного числа, или сообщает что третьей цифры нет.
+
+int FindThirdDigit(int num)
+{
+int third = 0;
+if (num >= 100)
+          {
+    while ( num >= 1000)
+             {
+                num = num / 10;
+             }
+        third = num %10;
+          }
+    else 
+          {
+             return -1;
+          }
+    return third;
+
+}
+Console.Write("Введите число -> ");
+int n = Convert.ToInt32(Console.ReadLine());
+int th = FindThirdDigit(n);
+Console.WriteLine("Третья цифра -> " + th);
